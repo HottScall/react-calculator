915eea72f1c7c025deaaf71c3ff5db8527e68925
Scott <Scotthall1983@hotmail.com>
2019-08-07 12:41:57 +0100

    Added a let wrapper and beforeEach tests to reduce boilerplate. Added a test to render a div in Calculator component and another test to ensure the app can read the Calc render component. Added state and basic methods to Calc to ensure div is redendered. Added import Calc to App to ensure App passes the read alc test

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import Calculator from "../Calculator/Calculator";
 import {
   isTaggedTemplateExpression,
   exportAllDeclaration,
@@ -8,8 +9,15 @@ import {
 } from "@babel/types";
 
 describe("App", () => {
+  let wrapper;
+  beforeEach(() => (wrapper = shallow(<App />)));
+
   it("should render a <div />", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should render the Calculator Component", () => {
+    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
   });
 });
